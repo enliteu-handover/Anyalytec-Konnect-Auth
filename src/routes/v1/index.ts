35a4f3a538c1185e3022fd1e08a85e61974902a7
vardhman <vardhman@crayond.co>
2023-03-28 10:42:43 +0530

    bugFix - middleware issue

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,27 +1,19 @@
+import { FastifyInstance } from "fastify";
 import fs from "fs";
 import path from "path";
 import { PRIVATE_FOLDER, PUBLIC_FOLDER } from "../../constants";
 
-const routesLoader = (fastify: any, sourceDir: string) => {
+const routesLoader = (fastify: FastifyInstance, sourceDir: string) => {
   fs.readdirSync(sourceDir, { withFileTypes: true })
     .filter((dirent: any) => dirent.isDirectory())
     .map((item: any) => item.name)
     .forEach(async (item: string) => {
       let route: any = await import(`${sourceDir}/${item}`);
-
-      //If routes are private they are authenticated with the jwt middleware
-      if (sourceDir.includes(PRIVATE_FOLDER)) {
-        fastify.addHook("onRequest", fastify.authenticate);
-        fastify.register(route.default, {
-          prefix: `/api/v1/${item}`,
-        });
-      } else {
-        fastify.register(route.default, { prefix: `/api/v1/${item}` });
-      }
+      fastify.register(route.default, { prefix: `/api/v1/${item}` });
     });
 };
 
-const routes = (fastify: any, _: any, done: any) => {
+const routes = (fastify: FastifyInstance, _: any, done: any) => {
   //Routes of Public API
   routesLoader(fastify, path.join(__dirname, PUBLIC_FOLDER));
   //Routes of Private API
--- src/routes/v1/private/auth/index.ts
+++ src/routes/v1/private/auth/index.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 
 const authRoutes = (fastify: FastifyInstance, _: any, done: any) => {
+  //If routes are private they are authenticated with the jwt middleware
+  fastify.addHook("preHandler", fastify.authenticate);
   fastify.get("/test", (req: FastifyRequest, reply: FastifyReply) => {
     reply.code(200).send({
       success: true,
