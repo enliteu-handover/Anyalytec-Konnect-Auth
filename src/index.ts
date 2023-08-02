308f2ce926d7b14c8a337a9931eb193cca334785
vardhman <vardhman@crayond.co>
2023-08-02 15:49:23 +0530

    bugFix:fastify logger issue

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,17 @@ import { fastify, FastifyInstance, FastifyReply } from "fastify";
 import { cpus } from "os";
 import fastifyAutoload from "@fastify/autoload";
 import path from "path";
-import { pinoLogger } from "./utils/logger";
 import routesV1 from "./routes/v1";
 import { ajvFilePlugin } from "@fastify/multipart";
 
 let PORT: number = Number(process.env.PORT) || 8080;
 
 const server: FastifyInstance = fastify({
-  logger: pinoLogger,
+  logger: true,
   ajv: {
     // Adds the file plugin to help @fastify/swagger schema generation
-    plugins: [ajvFilePlugin]
-  }
+    plugins: [ajvFilePlugin],
+  },
 });
 
 //Auto Loading Plugins in Fastify
@@ -45,12 +44,12 @@ const appStart = async () => {
 };
 
 if (cluster.isPrimary) {
-  pinoLogger.info(`Primary process running on ID : ${process.pid}`);
+  server.log.info(`Primary process running on ID : ${process.pid}`);
   for (let index = 0; index < numCPUs; index++) {
     cluster.fork();
   }
   cluster.on("exit", (worker: any, code: number, signal: string) => {
-    pinoLogger.error(
+    server.log.error(
       `Worker process ${worker.process.pid} exited with ${code} : ${signal}`
     );
     cluster.fork();
--- src/utils/logger.ts
+++
@@ -1,5 +0,0 @@
-import pino from "pino";
-
-const pinoLogger = pino({ level: "info" });
-
-export { pinoLogger };
